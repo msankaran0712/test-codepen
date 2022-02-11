614a9794842dd6006137e849087028673c97586b
Mahalakshmi Sankaran <50131391+msankaran0712@users.noreply.github.com>
2022-02-11 18:18:01 +0530

    Update import-react-bootstrap.js

--- import-react-bootstrap.js
+++ import-react-bootstrap.js
@@ -10,3 +10,16 @@ const Jumbotron = ReactBootstrap.Jumbotron;
 const Glyphicon = ReactBootstrap.Glyphicon;
 const Tooltip = ReactBootstrap.Tooltip;
 const Modal = ReactBootstrap.Modal;
+
+
+export Grid
+export Row
+export Col
+export Input
+export Button
+export ButtonToolbar
+export OverlayTrigger
+export Jumbotron
+export Glyphicon
+export ToolTip
+export Modal
